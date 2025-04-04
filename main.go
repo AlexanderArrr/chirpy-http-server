80d68394ba11eabcd23b0742110dfcc53dd4dead
AlexanderArrr <alexanderaretzger@gmail.com>
2025-04-04 22:43:42 +0200

    /app & /healthz endpoints

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	srvMux := http.NewServeMux()
-	srvMux.Handle("/", http.FileServer(http.Dir(".")))
+	srvMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	srvMux.HandleFunc("/healthz", handlerReadiness)
+
 	srv := &http.Server{
-		Addr:    "localhost:8080",
+		Addr:    "localhost:" + port,
 		Handler: srvMux,
 	}
 
-	srv.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }

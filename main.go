472d8682db85fa1e35e1936e845960a79d5e4f2e
AlexanderArrr <alexanderaretzger@gmail.com>
2025-04-06 03:22:54 +0200

    ChirpValidate added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -19,10 +20,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
-	metricsOutput := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	metricsOutput := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
 	w.Write([]byte(metricsOutput))
 }
 
@@ -43,9 +49,10 @@ func main() {
 
 	srvMux := http.NewServeMux()
 	srvMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	srvMux.HandleFunc("GET /healthz", handlerReadiness)
-	srvMux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	srvMux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	srvMux.HandleFunc("GET /api/healthz", handlerReadiness)
+	srvMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	srvMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	srvMux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	srv := &http.Server{
 		Addr:    "localhost:" + port,
@@ -60,4 +67,33 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		Valid: true,
+	})
+
 }

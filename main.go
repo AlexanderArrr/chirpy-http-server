aed4c269261a6c3c051ba1597267c97e015fdf41
AlexanderArrr <alexanderaretzger@gmail.com>
2025-04-06 03:41:42 +0200

    profanity filter added

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -67,33 +66,4 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type returnVals struct {
-		Valid bool `json:"valid"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
-		return
-	}
-
-	const maxChirpLength = 140
-	if len(params.Body) > maxChirpLength {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
-	})
-
 }
---
+++ validate.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type returnVals struct {
+		Cleaned_body string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+
+	cleanedBody := cleanChirp(params.Body)
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		Cleaned_body: cleanedBody,
+	})
+
+}
+
+func cleanChirp(body string) string {
+	var profanity []string
+	profanity = append(profanity, "kerfuffle", "sharbert", "fornax")
+	splitString := strings.Split(body, " ")
+	for i, ch := range splitString {
+		for _, prof := range profanity {
+			if strings.ToLower(ch) == prof {
+				splitString[i] = "****"
+			}
+		}
+	}
+	return strings.Join(splitString, " ")
+}
